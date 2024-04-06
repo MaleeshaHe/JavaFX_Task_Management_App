4b922fc31aa15f304a1e0d51fede8e83a2f21033
Maleesha Herath <malishaherath77@gmail.com>
2024-04-06 09:22:02 +0530

    Add user session file

---
+++ src/main/java/com/taskmanagement/task_management_app/UserSession.java
@@ -0,0 +1,59 @@
+package com.taskmanagement.task_management_app;
+
+import java.util.Set;
+
+public final class UserSession {
+
+    private static UserSession instance;
+
+    private static String userName;
+    private static String userTgNum;
+    private Set<String> privileges;
+    private static String depId;
+
+    private UserSession(String userName, String userTgNum,Set<String> privileges,String depId) {
+        this.userName = userName;
+        this.userTgNum = userTgNum;
+        this.privileges = privileges;
+        this.depId=depId;
+    }
+
+    public static UserSession getInstance(String userName, String userTgNum, Set<String> privileges,String depId) {
+        if(instance == null) {
+            instance = new UserSession(userName, userTgNum, privileges,depId);
+        }
+        return instance;
+    }
+
+    public static String getUserName() {
+        return userName;
+    }
+
+    public static String getUserTgNum() {
+        return userTgNum;
+    }
+
+    public Set<String> getPrivileges() {
+        return privileges;
+    }
+
+    public static String getUserDepId(){
+        return depId;
+    }
+
+    public static void cleanUserSession() {
+        userName = null;
+        userTgNum = null;
+        instance = null;
+        depId = null;
+
+    }
+
+    @Override
+    public String toString() {
+        return "UserSession{" +
+                "userName='" + userName + '\'' +
+                ", privileges=" + privileges +
+                '}';
+    }
+}
